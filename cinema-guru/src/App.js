2673d59d112526d72abb50232808e669961b5271
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-06-13 13:55:24 +0200

    Initialize the main App component

--- cinema-guru/src/App.js
+++ cinema-guru/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const headers = {authorization: `Bearer ${accessToken}`}
+    if (accessToken) {
+      axios.post('http://localhost:8000/api/auth', {}, { headers })
+      .then((res) => {
+          setIsLoggedIn(true);
+          setUserUsername(res.data.username);
+      }).catch(() => {
+        setIsLoggedIn(false);
+        setUserUsername("");
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      
-      
+      {isLoggedIn && <Dashboard setIsLoggedIn={setIsLoggedIn} userUsername={userUsername}/>}
+      {!isLoggedIn && <Authentication setIsLoggedIn={setIsLoggedIn} setUserUsername={setUserUsername}/>}
     </div>
   );
 }
-
-export default App;
